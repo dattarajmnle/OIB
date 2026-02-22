a7a45ffe5a0312b2015873a024b5a79b7ae6bfda
Dattaraj Anandrao Manale <rajmanale86300@gmail.com>
2026-02-22 11:09:00 +0530

    commit-2
    Addition of two numbers

---
+++ sum.java
@@ -0,0 +1,9 @@
+public class Sum{
+    public static void main(String [] args){
+        System.out.println(sum(10,20));
+    }
+    public static int sum(int a,int b){
+        int res = a+b;
+        return res;
+    }
+}
