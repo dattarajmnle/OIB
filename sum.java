1f753dd167bfd10bd1f0f4c27e090732f5984922
Dattaraj Anandrao Manale <rajmanale86300@gmail.com>
2026-02-22 11:10:43 +0530

    commit-3
    addition of three numbers

--- sum.java
+++ sum.java
@@ -1,9 +1,9 @@
 public class Sum{
     public static void main(String [] args){
-        System.out.println(sum(10,20));
+        System.out.println(sum(10,20,30));
     }
-    public static int sum(int a,int b){
-        int res = a+b;
+    public static int sum(int a,int b,int c){
+        int res = a+b+c;
         return res;
     }
 }
